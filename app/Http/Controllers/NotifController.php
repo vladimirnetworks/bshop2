c9e3812385a4cfa19849c5f928478790c64f6b27
vladimirnetworks <vladimirnetworks@gmail.com>
2022-01-01 14:35:48 +0330

    update using fastpush

--- app/Http/Controllers/NotifController.php
+++ app/Http/Controllers/NotifController.php
@@ -95,8 +95,8 @@ class NotifController extends Controller
         $SecretKey = "yech!vetb0em";
         $LineNumber = "30002176999072";
         $APIURL = "https://ws.sms.ir/";
-        $MobileNumbers = array('09332999594');
-        $Messages = array('بهکیانا سفارش جدید');
+        $MobileNumbers = array('09332999594','09163686265');
+        $Messages = array('بهکیانا سفارش جدید','بهکیانا سفارش جدید');
 
         @$SendDateTime = date("Y-m-d")."T".date("H:i:s");
         $SmsIR_SendMessage = new \App\bensms\smsir($APIKey, $SecretKey, $LineNumber, $APIURL);
