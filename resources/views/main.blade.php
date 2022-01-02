9fcab27b6a293e595f7eb0249ac5b0eeed4bbd00
vladimirnetworks <vladimirnetworks@gmail.com>
2022-01-02 13:51:37 +0330

    update using fastpush

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -54,7 +54,7 @@ width: 100%;
 display: flex;justify-content: center;
     align-items: center;
 ">
-<div style="width:12wv">
+<div style="width:12vw">
   <a referrerpolicy="origin" target="_blank" href="https://trustseal.enamad.ir/?id=244136&amp;Code=NAYvp5VDiHI1pstposy1"><img style="width:100%" referrerpolicy="origin" src="https://Trustseal.eNamad.ir/logo.aspx?id=244136&amp;Code=NAYvp5VDiHI1pstposy1" alt="" style="cursor:pointer" id="NAYvp5VDiHI1pstposy1"></a>
 
 </div>
