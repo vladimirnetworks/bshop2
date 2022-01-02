b84fe3c1a46acd91708de6d4aa3af793b6f9381d
vladimirnetworks <vladimirnetworks@gmail.com>
2022-01-02 14:07:53 +0330

    update using fastpush

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -68,11 +68,11 @@ display: flex;justify-content: center;
   </div>
 
  <script>
-   $("body").css("margin-top",$("#topxbar").height()+"px");
+   $("body").css("margin-top",($("#maintopxbar").offset()['top']+$("#topxbar").height())+"px");
 
    window.addEventListener('resize', function () {
      
-    $("body").css("margin-top",$("#topxbar").height()+"px");
+    $("body").css("margin-top",($("#maintopxbar").offset()['top']+$("#topxbar").height())+"px");
 
    })
  </script> 
