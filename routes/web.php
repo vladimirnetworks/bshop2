5f6a3f35eb8c7f072ae9c9a808f138ddfa249aaa
vladimirnetworks <vladimirnetworks@gmail.com>
2022-02-01 12:11:17 +0330

    update using fastpush

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', "App\Http\Controllers\mainPageController@index")->middleware('tokin');
+Route::get('/', "App\Http\Controllers\mainPageController@index2")->middleware('tokin');
 
 Route::get('/cat/{cat}', "App\Http\Controllers\mainPageController@index")->middleware('tokin');
 
