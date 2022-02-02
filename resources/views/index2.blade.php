15d7fdc2af028d628d4d16953d58be30fbd67449
vladimirnetworks <vladimirnetworks@gmail.com>
2022-02-02 09:26:09 +0330

    update using fastpush

--- resources/views/index2.blade.php
+++ resources/views/index2.blade.php
@@ -2,7 +2,7 @@
 
 @section('main')
 
-<div style="z-index: 1003;position: fixed;background-color: red;top: 0px;left: 0px;text-align: center;width: 100%;height: 11vh;display: inline;">
+<div class="top">
  
 </div>
 
