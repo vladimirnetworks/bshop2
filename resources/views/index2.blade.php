7f9363f2895a558167f4cb297b3aa0483a23ced6
vladimirnetworks <vladimirnetworks@gmail.com>
2022-02-02 17:50:03 +0330

    update using fastpush

--- resources/views/index2.blade.php
+++ resources/views/index2.blade.php
@@ -4,7 +4,7 @@
 
 <div class="top">
    
-    <a referrerpolicy="origin" target="_blank" href="https://trustseal.enamad.ir/?id=244136&Code=NAYvp5VDiHI1pstposy1" style="height: 95%;
+    <a style="height: 100%;" referrerpolicy="origin" target="_blank" href="https://trustseal.enamad.ir/?id=244136&Code=NAYvp5VDiHI1pstposy1" style="height: 95%;
 "><img style="height: 100%;" referrerpolicy="origin" src="https://Trustseal.eNamad.ir/logo.aspx?id=244136&Code=NAYvp5VDiHI1pstposy1" alt="" id="NAYvp5VDiHI1pstposy1"></a>
 
     <input style="height: 60%;flex-grow: 2;" placeholder="جستجو در محصولات" />
