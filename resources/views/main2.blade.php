808aef68584176c84ce73d0d0ef3637838a6d2d7
vladimirnetworks <vladimirnetworks@gmail.com>
2022-02-02 09:25:43 +0330

    update using fastpush

--- resources/views/main2.blade.php
+++ resources/views/main2.blade.php
@@ -7,11 +7,27 @@
   <title>{{$pageTitle}}</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="author" content="">
-  <link rel="stylesheet" href="css/style.css">
+
   <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+  <style>
+    .top {
+      z-index: 1003;
+      position: fixed;
+      background-color: red;
+      top: 0px;
+      left: 0px;
+      text-align: center;
+      width: 100%;
+      height: 11vh;
+      max-height: 4rem;
+      min-height: 3rem;
+      display: inline;
+    }
+  </style>
 </head>
 
 <body>
- @yield('main') 
+  @yield('main')
 </body>
+
 </html>
