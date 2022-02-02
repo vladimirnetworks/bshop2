a1c8ada92d8c99a1d716560f2d69a11ca5a10427
vladimirnetworks <vladimirnetworks@gmail.com>
2022-02-02 17:48:51 +0330

    update using fastpush

--- resources/views/index2.blade.php
+++ resources/views/index2.blade.php
@@ -3,7 +3,10 @@
 @section('main')
 
 <div class="top">
-    <a referrerpolicy="origin" target="_blank" href="https://trustseal.enamad.ir/?id=244136&amp;Code=NAYvp5VDiHI1pstposy1"><img style="width:100%" referrerpolicy="origin" src="https://Trustseal.eNamad.ir/logo.aspx?id=244136&Code=NAYvp5VDiHI1pstposy1" alt="" id="NAYvp5VDiHI1pstposy1"></a>
+   
+    <a referrerpolicy="origin" target="_blank" href="https://trustseal.enamad.ir/?id=244136&Code=NAYvp5VDiHI1pstposy1" style="height: 95%;
+"><img style="height: 100%;" referrerpolicy="origin" src="https://Trustseal.eNamad.ir/logo.aspx?id=244136&Code=NAYvp5VDiHI1pstposy1" alt="" id="NAYvp5VDiHI1pstposy1"></a>
+
     <input style="height: 60%;flex-grow: 2;" placeholder="جستجو در محصولات" />
   <img style="height: 60%;" src="https://s6.uupload.ir/files/mag_91qh.png">
 </div>
