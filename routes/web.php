6d35cc958bd69b8467ba5aaa7807db4beb1a3138
vladimirnetworks <vladimirnetworks@gmail.com>
2022-02-21 19:15:52 +0330

    update using fastpush

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/android-web-appver', function () {
 });
 
 Route::get('/androidwebapp-1', "App\Http\Controllers\mainPageController@index33");
-
+Route::get('/androidwebapp-2', "App\Http\Controllers\mainPageController@index33");
 
 
 Route::get('/', "App\Http\Controllers\mainPageController@index")->middleware('tokin');
