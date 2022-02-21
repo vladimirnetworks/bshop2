562626423966a440815cbf00ab9ba5fcc289f9ae
vladimirnetworks <vladimirnetworks@gmail.com>
2022-02-21 19:15:07 +0330

    update using fastpush

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/android-web-appver', function () {
-    return "1";
+    return "2";
 });
 
 Route::get('/androidwebapp-1', "App\Http\Controllers\mainPageController@index33");
