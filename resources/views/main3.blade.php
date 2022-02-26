8a179d0b17105dcaca1fa1b8166c93fbe888317b
vladimirnetworks <vladimirnetworks@gmail.com>
2022-02-26 18:38:13 +0330

    update using fastpush

--- resources/views/main3.blade.php
+++ resources/views/main3.blade.php
@@ -31,7 +31,7 @@
     </div>
 
     <div style="direction: rtl;text-align:center;margin-top: 1rem;">
-      <a referrerpolicy="origin" target="_blank" href="https://trustseal.enamad.ir/?id=244136&Code=NAYvp5VDiHI1pstposy1"><img style="width:100%" referrerpolicy="origin" src="https://Trustseal.eNamad.ir/logo.aspx?id=244136&Code=NAYvp5VDiHI1pstposy1" alt="" style="cursor:pointer" id="NAYvp5VDiHI1pstposy1"></a>
+      <a referrerpolicy="origin" target="_blank" href="https://trustseal.enamad.ir/?id=244136&Code=NAYvp5VDiHI1pstposy1"><img  referrerpolicy="origin" src="https://Trustseal.eNamad.ir/logo.aspx?id=244136&Code=NAYvp5VDiHI1pstposy1" alt="" style="cursor:pointer" id="NAYvp5VDiHI1pstposy1"></a>
     </div>
   </div>
 
