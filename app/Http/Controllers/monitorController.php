fff9971ac1ccea0f7ac27c0d09ca1289ad6ee7b8
vladimirnetworks <vladimirnetworks@gmail.com>
2022-03-23 21:29:57 +0430

    update using fastpush

--- app/Http/Controllers/monitorController.php
+++ app/Http/Controllers/monitorController.php
@@ -35,7 +35,7 @@ class monitorController extends Controller
      public function Monitoring(Request $request)
      {
        
-      $latest = monitor::orderBy('id', 'DESC')->paginate(20, ['*'], 'page', $request->page);
+      $latest = monitor::where("url","not like","%/api/admin/%")->orderBy('id', 'DESC')->paginate(20, ['*'], 'page', $request->page);
 
      // $latest = DB::select("SELECT useragent FROM monitor where `liteauth_id` > 0 GROUP BY `liteauth_id` ORDER BY `liteauth_id` DESC limit 50");
       return  $latest ;
