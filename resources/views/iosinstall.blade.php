b7f9f1417ce7f30a633fb1dace8e8c48e0417018
vladimirnetworks <vladimirnetworks@gmail.com>
2022-03-30 12:35:15 +0430

    update using fastpush

--- resources/views/iosinstall.blade.php
+++ resources/views/iosinstall.blade.php
@@ -1,6 +1,6 @@
 @extends('mobileappinstall')
  @section('links')
  <a style="" class="xa" href="https://anardoni.com/ios/app/mIk0JiHtn"><img style="max-width:100%;max-height:100%;" src="/icons/anardoni.png"/></a>
-
+ <a style="" class="xa" href="https://sibche.com/applications/supermarket?utm_source=%D8%B3%D9%88%D9%BE%D8%B1%D9%85%D8%A7%D8%B1%DA%A9%D8%AA&utm_medium=referral&utm_campaign=download_badge"><img style="max-width:100%;max-height:100%;" src="/icons/sibche.png"/></a>
  <a class="xa" href="/webapp">ورود به نسخه‌ی مرورگر</a>
 @stop
